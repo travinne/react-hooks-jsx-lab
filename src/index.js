ba214bc454445ee9887e13c0f24ead6df5d7f15c
travinne <travinne.mutahi@moringaschool.com>
2025-04-28 09:43:47 +0300

    react-hooks-jsx-lab

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import NavBar from "./components/NavBar";
+import Home from "../components/Home";
+
+function (){
+    return (
+        <div>
+        <NavBar/>    
+        <About/>
+        <Home/>
+
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
